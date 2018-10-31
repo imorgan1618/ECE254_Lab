c4ec21089b57e1127f389f486856c5af9b40532b
Lynnea Yee Lin Lurline Tan <lynneatan@gmail.com>
2018-10-31 17:10:33 -0400

    Initial commit

--- lab3/starter/src/threads/produce.c
+++ lab3/starter/src/threads/produce.c
@@ -13,15 +13,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <math.h>
+#include <pthread.h>
 
 double g_time[2];
-
+int produced;
+int num_p;
+int num;
 
 int main(int argc, char *argv[])
 {
-	int num;
 	int maxmsg;
-	int num_p;
 	int num_c;
 	int i;
 	struct timeval tv;
@@ -35,7 +36,21 @@ int main(int argc, char *argv[])
 	maxmsg = atoi(argv[2]); /* buffer size                */
 	num_p = atoi(argv[3]);  /* number of producers        */
 	num_c = atoi(argv[4]);  /* number of consumers        */
+	num_total_t = num_p + num_c;
 
+	pthread_t theads[num_total_t];
+
+	for (int i = 0; i < num_total_t -num_c; i++){
+		int* tmp = malloc(sizeof(int));
+		*tmp = ij;
+		pthread_create(&(threads[i], NULL, producer, tmp);
+	
+	}
+	
+	for (int j = num_c; j < num_total_t; j++ ){
+		pthread_create(&(threads[j], NULL, producer, NULL);
+	
+	}
 
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
@@ -47,4 +62,22 @@ int main(int argc, char *argv[])
     printf("System execution time: %.6lf seconds\n", \
             g_time[1] - g_time[0]);
 	exit(0);
+}
+
+void* consumer(){
+
+}
+
+void* producer(void* argument){
+	int* id = (int*) argument;
+	
+	for (i=0; i < num; i++){
+		//check if the buffer is full
+		if ((i%num_p) == *id){
+			//lock something
+			// add iterm to the buffer
+			// unlock something
+			
+		}
+	}
 }
